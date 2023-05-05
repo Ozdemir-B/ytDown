1bae1543ef5e286f115f8274b98f4f5d72f3cbeb
Ozdemir-B <berkayozdemirv2@gmail.com>
2023-05-06 02:54:03 +0300

    v1

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, send_file, render_template
+from io import BytesIO
+from youtube import *
+import uuid
+import os
+
+
+app = Flask("__main__")
+
+yt = YouTube()
+
+last_vid = ""
+
+
+#@app.route("/download",methods=["GET","POST"])
+def down(url:str):
+    global last_vid
+    print("last vid : "+last_vid)
+    ###url = request.args["url"]
+    try:
+        os.remove(last_vid)
+    except Exception as e:
+        print(e)
+    print(url)
+    url=url.split("?")[0]
+    #url = f"https://www.youtube.com/{url}"
+    myuuid = str(uuid.uuid4())
+    result,title = yt.download_video(url,myuuid)
+    last_vid = result
+    print("result:::")
+    print(result)
+    if result:
+        last_img = result
+        return (result,title,myuuid)
+    else:
+        return (None , None, None)
+    
+    
+@app.route("/download",methods=["GET","POST"])
+def download():
+    print(request.values)
+    print(request.form["inputUrl"])
+    keyword = request.form["inputUrl"]
+    if keyword:
+        try:
+            result,vid_id = down(keyword)
+            return send_file(
+                result,
+                as_attachment=True,
+                attachment_filename=f"{vid_id}.mp4",
+                mimetype="video/mp4",
+            )
+        except Exception as e:
+            print(e)
+            return "Error"
+
+    
+@app.route("/home",methods=["GET","POST"])
+def home():
+    
+    if request.method == 'POST':
+        print(request.values)
+        keyword = request.form["inputUrl"]
+        print(keyword)
+        if keyword:
+            try:
+                result,title,vid_id = down(str(keyword))
+                return render_template("index.html",video=[result,title])
+                return send_file(
+                    result,
+                    as_attachment=True,
+                    attachment_filename=f"{vid_id}.mp4",
+                    mimetype="video/mp4",
+                )
+            except Exception as e:
+                print(e)
+                return "Error"
+    else:
+        return render_template("index.html")
+    return "Error2"
+    
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=4000,debug=True)
---
+++ youtube.py
@@ -0,0 +1,83 @@
+"""Set of functions for working with YouTube"""
+
+from os.path import join
+from typing import Any, NamedTuple
+
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import YoutubeDLError
+
+
+
+
+
+class VideoInfo(NamedTuple):
+    """Presents information about the video"""
+
+    description: str
+    url: str
+
+
+class YouTube:
+    """Describes methods for working with YouTube videos"""
+
+    
+    def _remove_unwanted_chars(string: str) -> str:
+        """Removes everything from the string except letters, numbers, spaces, hyphens, and underscores"""
+        processed_string: str = ""
+        for char in string[:100]:
+            if char.isalnum() or char == "-" or char == "_":
+                processed_string += char
+            elif char.isspace() and (not processed_string or not processed_string[-1].isspace()):
+                processed_string += char
+        return processed_string
+
+    
+    def download_video(self, youtube_watch_url: str,id:str) -> Any:
+        """
+        Downloads the best video stream from the YouTube Shorts link
+        Note: Do not use named arguments when calling this method
+        Args:
+            youtube_watch_url (): link to YouTube Shorts video
+        Returns:
+            The path to the uploaded video file, or None if the original video file is a live broadcast
+        """
+        options: dict = {
+            "format": "best",
+            "geo_bypass": True,
+            "noplaylist": True,
+            "noprogress": True,
+            "quiet": True,
+        }
+        ydl: YoutubeDL = YoutubeDL(params=options)
+
+        try:
+            # Getting information about the video
+            video_info = ydl.extract_info(youtube_watch_url, download=False)
+            duration: int | None = video_info.get("duration")
+
+            if duration and duration <= 60:
+                # Set save folder and file name
+                #print(video_info)
+                title: str = video_info.get("title")
+                print(title)
+                path_to_file: str = f"static/{id}.mp4"
+                params: dict = getattr(ydl, "params")
+                params.update({"outtmpl": {"default": path_to_file}})
+                setattr(ydl, "params", params)
+                #print(params)
+                # Load a video stream
+                ydl.download(youtube_watch_url)
+                
+                print("ended...")
+                return f"{path_to_file}", title
+
+        except YoutubeDLError as ex:
+            print("Error when loading video: %s", repr(ex))
+
+        return None
+
+
+if __name__ == "__main__":
+    youtube = YouTube()
+    x=youtube.download_video("https://www.youtube.com/shorts/u1c9TRKkYgE","some_name")#"https://www.youtube.com/watch?v=Llr2dcd-VBo")#"https://www.youtube.com/shorts/u1c9TRKkYgE")
+    #print(x)
